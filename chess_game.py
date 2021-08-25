5c1c1b62ed632906077691d2741a065b8d6f5669
CyStorm <>
2021-08-24 21:45:55 -0400

    move logic only legal

--- chess_game.py
+++ chess_game.py
@@ -1,17 +1,30 @@
+import chess
 
 class ChessGame():
     '''The game class, storing where peices are and information about gamestate
     '''
     def __init__(self):
-        self.board = [
-            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
-            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
-            ["bR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
-        ]
+        self.board = chess.Board()  # main python-chess board object
         self.move_log = []
         self.white_to_move = True
+
+    def move_piece(self, start_square, end_square, promotion=None):
+        '''Moves piece from starting square to ending square
+        '''
+        move = chess.Move(start_square, end_square, promotion)
+        if (move in self.board.legal_moves):
+            self.board.push(move)
+        else:
+            print("Not legal")
+
+if (__name__ == "__main__"):
+    game = ChessGame()
+    print(game.board)
+    while (True):
+        start = int(input("start "))
+        end = int(input("end "))
+        if (start == 99):
+            break
+        game.move_piece(start, end)
+        print(game.board)
+    pass
