0206bab918785f9317a1a011b58d7e39165d74cb
CyStorm <>
2021-09-16 21:01:08 -0400

    added code to compute material weight

--- engine_base.py
+++ engine_base.py
@@ -6,6 +6,14 @@ class EngineBase():
     def __init__(self, game: ChessGame, side: chess.Color):
         self.game = game
         self.side = side
+        self.material_weight = {
+            chess.KING: 0,
+            chess.QUEEN: 9,
+            chess.ROOK: 5,
+            chess.KNIGHT: 3,
+            chess.BISHOP: 3,
+            chess.PAWN: 1,
+        }
 
     def generate_next_move(self):
         '''Uses the current board state to generate the next move
@@ -17,3 +25,39 @@ class EngineBase():
         if (self.game.white_to_move == self.side):
             move = self.generate_next_move()
             self.game.play_move(move)
+
+    def get_self_material_value(self):
+        '''Computes the numerical material values of all of our own peices
+        '''
+        total = 0
+        pieces = self.game.board.piece_map()
+        for piece in pieces.values():
+            if (piece.color == self.side):
+                total += self.material_weight[piece.piece_type]
+        return total
+
+    def get_opponent_material_value(self):
+        '''Computes the numerical material values of all of opponent's peices
+        '''
+        total = 0
+        pieces = self.game.board.piece_map()
+        for piece in pieces.values():
+            if (piece.color != self.side):
+                total += self.material_weight[piece.piece_type]
+        return total
+
+if (__name__ == "__main__"):
+    '''debug code for this file
+    '''
+    material_weight = {
+        chess.KING: 0,
+        chess.QUEEN: 9,
+        chess.ROOK: 5,
+        chess.KNIGHT: 3,
+        chess.BISHOP: 3,
+        chess.PAWN: 1,
+    }
+    print(material_weight[chess.KING])
+    print(material_weight[chess.QUEEN])
+    for piece, val in material_weight.items():
+        print(piece, val)
